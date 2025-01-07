ecc64e907c8852b63d139577822a81e96f3be312
johnfinley <johnfinleybremerton@gmail.com>
2025-01-06 21:16:33 -0700

    Initial commit: Basic Project Structure - Created basic frontend and backend folders - Initialized version control

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import app from "./app.js"
+import { config } from "dotenv"
+
+config({ path: 'backend/config/config.env'})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on PORT ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
+})
