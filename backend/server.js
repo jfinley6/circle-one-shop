6e9800f850df398df4b76883814dfcf28313d58e
johnfinley <johnfinleybremerton@gmail.com>
2025-01-08 16:41:53 -0700

    Refactor: Replace import statements with require for consistency with Express docs
    - Updated `app.js` to use `require` for importing Express
    - Updated `server.js` to use `require` for importing `app` and `dotenv`
    - Ensured compatibility with Node.js CommonJS module syntax

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import app from "./app.js"
-import { config } from "dotenv"
+const app = require("./app")
+const dotenv = require("dotenv")
 
-config({ path: 'backend/config/config.env'})
+dotenv.config({ path: 'backend/config/config.env'})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
