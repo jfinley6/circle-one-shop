d80c0b1658c7c0a682337e77f8939edb4d84da02
johnfinley <johnfinleybremerton@gmail.com>
2025-01-12 14:05:56 -0700

    feat: Add products route and controller
    - Created `productController.js` with a `getProducts` method to return a JSON response for all products
    - Created `products.js` route file to define the `/products` GET route
    - Updated `app.js` to include:
      - `express.json()` middleware for parsing JSON requests
      - Imported and mounted the `products` routes at `/api/v1/products`

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,11 @@ const express = require('express')
 
 const app = express();
 
+app.use(express.json())
+
+// Import all routes
+const products = require("./routes/products")
+
+app.use("/api/v1", products)
+
 module.exports =  app
